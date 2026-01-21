6f49d185e0e09052822b5a7c95a8bec1c885ae1b
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-21 12:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+let link = document.querySelector("a");
+let strong = document.querySelectorAll('strong');
+
 function highlight() {
     //Write your code here
-
+	link.addEventListener("mouseover", ()=>{
+		strong.forEach(word => {
+			word.style.color = "green"
+		})
+		
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	link.addEventListener("mouseout", ()=>{
+		strong.forEach(word => {
+			word.style.color = "black"
+		})
+		
+	})
     
 }
