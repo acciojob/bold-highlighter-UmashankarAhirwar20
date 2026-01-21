3f1792c589314499538aeb6c96115100f2652e54
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2026-01-21 13:03:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,18 @@ let strong = document.querySelectorAll('strong');
 
 function highlight() {
     //Write your code here
-	link.addEventListener("mouseover", ()=>{
 		strong.forEach(word => {
 			word.style.color = "rgb(0, 128, 0)"
 		})
-		
-	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-	link.addEventListener("mouseout", ()=>{
 		strong.forEach(word => {
 			word.style.color = "rgb(0, 0, 0)"
 		})
 		
-	})
     
 }
